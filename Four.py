af668c802926f0428e013b0a393e010a50131fce
STRiiXER <127283772+striixervro@users.noreply.github.com>
2023-10-18 22:34:55 +0530

    Four

---
+++ Four.py
@@ -0,0 +1,13 @@
+# Operators 
+
+# Arithmetic Operators
+# + - * / % // 
+#  // ---> this is float division operator
+
+print(10+10)
+print(10-10)
+print(10*18)
+print(10/10)
+print(10%10)
+print(10.10//10)
+print(10**10)
