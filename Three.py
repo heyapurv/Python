7d4780a544b1d7d680d8c952e56395643902d36e
STRiiXER <127283772+striixervro@users.noreply.github.com>
2023-10-18 22:30:19 +0530

    Three

---
+++ Three.py
@@ -0,0 +1,20 @@
+# Variable and Data Types 
+# variable is like a container that holds data
+a = 1234.2
+print (a)
+
+b = "xyz"
+print(b)
+
+c = True
+d = None
+e = complex(3,2)
+# Data type specifies the type of value a variable holds.
+
+print("The type of a is:", type(a))
+print("The type of b is:", type(b))
+print("The type of c is:", type(c))
+print("The type of d is:", type(d))
+print("The type of c is:", type(e))
+
+# In python, everything is an object
