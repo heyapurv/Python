4bca901dcc94cfcc86058bc6c99288b9731a91ee
STRiiXER <127283772+striixervro@users.noreply.github.com>
2023-10-18 22:41:11 +0530

    EX1SOl

---
+++ Ex1_Calculator.py
@@ -0,0 +1,6 @@
+a = 10 
+b = 2
+print("The value of",a, "+",b, "is:", a+b)
+print("The value of",a, "-",b, "is:", a-b)
+print("The value of",a, "/",b, "is:", a/b)
+print("The value of",a, "*",b, "is:", a*b)
