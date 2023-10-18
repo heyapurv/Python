0f812f3c16329428aaa210241e76603707a1bbb0
STRiiXER <127283772+striixervro@users.noreply.github.com>
2023-10-18 22:13:33 +0530

    Two

---
+++ Two.py
@@ -0,0 +1,11 @@
+# comments, Escape sequences and print statement
+# Escape sequence character --> \n new line
+print("hey my name is xyz \n and now i'm going to learn python")
+
+print("\"Hello there!\"")  #you can also add a comment like this 
+
+#  \" \'
+
+print("hey",6,8, sep="~", end="001") # sep is used to seperate the characters by the value given in the sep
+# end is uesd to add the value provided in the end at the end of the print value
+
