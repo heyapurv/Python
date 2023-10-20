8aa2dac37cac331f10831061563dc1fc73a79623
STRiiXER <127283772+striixervro@users.noreply.github.com>
2023-10-20 14:53:27 +0530

    six

---
+++ Six.py
@@ -0,0 +1,8 @@
+# a = input("Enter your name: ")
+# print("My name is: ",a)
+
+x = input("input 1st no: ")
+y = input("input second no. : ")
+print (int(x)+ int(y))
+
+# anything taken as input will be considered as string bydefault
