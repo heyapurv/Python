91db72d54f7ba4b7276c44eda43d21fa5f80b34f
STRiiXER <127283772+striixervro@users.noreply.github.com>
2023-10-20 15:01:48 +0530

    seven

---
+++ seven.py
@@ -0,0 +1,19 @@
+# STRINGS 
+
+name = "XYZ"
+dost = "bcd"
+bestDost = "aaaa"
+print(name,"\n",dost)
+
+print('''HEy there
+      how are you
+      tell me''')
+
+print(name[0])
+print(name[1])
+print(name[2])
+# print(name[3]) # throws an error
+
+print( "lets use a for loop")
+for characters in dost:
+    print(characters)
